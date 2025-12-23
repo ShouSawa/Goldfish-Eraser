ed08020a616e5abe0fe105177cb7c09b8dd363b5
ShouSawa <shotamain1211@gmail.com>
2025-12-23 13:35:24 +0900

    Lチカプログラムの修正

--- LED-flashing.py
+++ LED-flashing.py
@@ -5,9 +5,8 @@ led = Pin('LED', Pin.OUT) # Pico W は 'LED' と設定
 
 for i in range(10):
     led.on()
-    time.sleep(0.5)
+    time.sleep(1)
     led.off()
-    time.sleep(0.5)
+    time.sleep(1)
 
 print("LEDチカチカを、10回やりました！")
-yy
